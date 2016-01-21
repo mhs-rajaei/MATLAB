3619af2f4ba6a4c9954408c8217178631c92ece7
Mohsen Rajaie <mrajaie1@gmail.com>
2016-01-22 02:49:49 +0330

    bad result, i wana remove unness codes

--- Neural Network/HW3/Google Net/AircraftClassifier.m
+++ Neural Network/HW3/Google Net/AircraftClassifier.m
@@ -3,7 +3,7 @@ clear
 close all;
 vl_setupnn;
 % load the pre-trained CNN
-cnnModel.net = load('F:\Documents\MATLAB\Neural Network\HW3\Google Net\Data\imagenet-vgg-verydeep-19.mat');
+cnnModel.net = load('F:\Documents\MATLAB\Neural Network\HW3\Google Net\Data\imagenet-vgg-f.mat');
 % %% Load images from folder
 % % Use imageSet to load images stored in pet_images folder
 % imset = imageSet('F:\Documents\MATLAB\Neural Network\HW3\Google Net\Data\Images','recursive');
@@ -30,7 +30,10 @@ cnnModel.net = load('F:\Documents\MATLAB\Neural Network\HW3\Google Net\Data\imag
 
 %% Load data from folder
 % Use imageSet to load images stored in pet_images folder
-imset = imageSet('F:\Documents\MATLAB\Neural Network\HW3\Google Net\Data\Images','recursive');
+% imset = imageSet('F:\Documents\MATLAB\Neural Network\HW3\Google Net\Data\Images','recursive');
+% imset = imageSet('F:\Documents\MATLAB\Neural Network\HW3\Google Net\Data\Airplane Images','recursive');
+imset = imageSet('F:\Documents\MATLAB\Neural Network\HW3\Google Net\Data\Pet Images','recursive');
+
 
 
 
@@ -53,7 +56,7 @@ summary(Labels) % Display class label distribution
 %% shuffeing images
 min_idx=1;
 max_idx=counter;
-idx = randperm(max_idx-min_idx+1,35);
+idx = randperm(max_idx-min_idx+1,40);
 % imshow(images2(1).contain);
 % pause;
 % Preallocate arrays with fixed size
@@ -100,42 +103,80 @@ training_Labels = training_Labels(idx,:);
 % end
 
 %%
-cnnModel.info.opts.batchSize = 5;
-cnnFeatures = cnnPredict(cnnModel,training_images);
 
+cnnModel.net.layers = cnnModel.net.layers(1:end-1);
+cnnModel.info.opts.batchSize = 10;
+% [~,cnnFeatures,~] = cnnPredict0(cnnModel,training_images);
+cnnFeatures = cnnPredict(cnnModel,training_images);
+t_cnnFeatures = cnnPredict(cnnModel,test_images);
 
 training_target = zeros(label_marix_size,1);
 for i=1:label_marix_size
-    if strcmpi(char(training_Labels(i)),'cat')
+    if strcmpi(char(training_Labels(i)),'0')
         training_target(i,1) = 0;
     else
         training_target(i,1) = 1;
     end
 end
 
-[test_label_marix_size,~] = size(training_Labels);
+[test_label_marix_size,~] = size(test_Labels);
 test_target = zeros(test_label_marix_size,1);
 for i=1:test_label_marix_size
-    if strcmpi(char(training_Labels(i)),'cat')
+    if strcmpi(char(training_Labels(i)),'0')
         test_target(i,1) = 0;
     else
         test_target(i,1) = 1;
     end
 end
+
+%% feature normalozation
+for i=1:label_marix_size
+
+
+    newMax = 1;
+    newMin = 0;
+    myImgNorm = (cnnFeatures(i,:) - min(cnnFeatures(i,:)))*...
+        (newMax - newMin)/(max(cnnFeatures(i,:)) - min(cnnFeatures(i,:))) + newMin;
+    cnnFeatures(i,:) = myImgNorm;
+end
+
+for i=1:test_label_marix_size
+
+
+    newMax = 1;
+    newMin = 0;
+    myImgNorm = (t_cnnFeatures(i,:) - min(t_cnnFeatures(i,:)))*...
+        (newMax - newMin)/(max(t_cnnFeatures(i,:)) - min(t_cnnFeatures(i,:))) + newMin;
+    t_cnnFeatures(i,:) = myImgNorm;
+end
+
+
+%%
+% run the CNN
+% for i=1:label_marix_size  
+%  res(i).net_feature = vl_simplenn(cnnModel.net, single(training_images(:,:,:,i))) ;
+% end
 %% Set inputs for MLP
-t_cnnFeatures = cnnPredict(cnnModel,test_images);
+% [~,t_cnnFeatures,~] = cnnPredict0(cnnModel,test_images);
+
 t_inputs = t_cnnFeatures;
 inputs = cnnFeatures;
 
 t_feature_target = test_target;
 feature_target = training_target;
 
-%% Learning MLP with PCA feature
+
+
+
+%% Learning MLP with Googl net feature
 
 google_net = 1;
 MR_MLP;
 
-
+%%
+% vl_simplenn_display(cnnModel.net) ;
+% run the CNN
+% res = vl_simplenn(net, im_) ;
 %% Train with NN toolbox in matlab if you want!
 % MLP_Inputs= [inputs];
 % Target = feature_target ;
