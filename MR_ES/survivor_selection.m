bc191e1d99746141f803a235a3269afb7135c26c
Mohsen Rajaie <mrajaie1@gmail.com>
2016-06-19 01:52:54 +0430

    remove objective_function and add fintess function

--- MR_ES/survivor_selection.m
+++ MR_ES/survivor_selection.m
@@ -1,5 +1,5 @@
 function [new_parents, new_sigma, new_alpha] = survivor_selection(sel_scheme, mu, lambda, ...
-    off_error, par_error, offspring, mut_sigma, parents, sigma, mut_alpha, alpha)
+    offspring_fitness, parent_fitness, offspring, mut_sigma, parents, sigma, mut_alpha, alpha)
 %% Beginning
 switch sel_scheme
   %% (mu, lambda)-survivor selection scheme
@@ -7,7 +7,7 @@ switch sel_scheme
     if (mu > lambda)
       error('The parents population size is greater than the offspring population size');
     end
-    err = off_error;
+    err = offspring_fitness;
     [xmin, idx] = sort(err);
     new_parents = offspring(:,idx(1:mu));
     new_sigma   = mut_sigma(idx(1:mu));
@@ -15,7 +15,7 @@ switch sel_scheme
 
   %% (mu + lambda)-survivor selection scheme
   case '+'
-    err         = [off_error par_error];    
+    err         = [offspring_fitness parent_fitness];    
     parents_plus_offspring        = [offspring parents];
     parents_plus_offspring_sigma    = [mut_sigma sigma];
     parents_plus_offspring_alpha    = [mut_alpha alpha];
