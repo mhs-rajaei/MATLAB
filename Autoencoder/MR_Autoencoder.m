dddf1cb866e4be10acc7f62280ab46f2a9c4827e
Mohsen Rajaie <mrajaie1@gmail.com>
2016-06-19 19:33:19 +0430

    Thaks ALLAH its working

--- Autoencoder/MR_Autoencoder.m
+++ Autoencoder/MR_Autoencoder.m
@@ -1,198 +1,202 @@
 %% =========== Start MR_Autoencoder =============
+close all;clear all;clc;
+%% Prepare Inputs:
 
+input_image=imread('F:\Documents\MATLAB\Data\Autoencoder\1.jpg');
+input_image = rgb2gray(input_image);
 
-clear all;
+hight = 227;
+width = 227;
 
-% load('F:\Documents\R\Kyoto\Processed Dataset\preprocessed\mtalab\processed_train_selected_normal_plus_attck.mat')
-% processed_train_selected_normal_plus_attck = processed_train_selected_normal_plus_attck;
-% clear('processed_test_selected_normal_plus_attck')
-% p6
-load('F:\Documents\R\Kyoto\Processed Dataset\preprocessed\mtalab\p9\train_kyoto_data.mat')
-processed_train_selected_normal_plus_attck = training_data;
-clear('training_data');
-%% preparing target labels
-% train targets
-train_labels = zeros(2,size(processed_train_selected_normal_plus_attck,1))';
-for i=1:size(processed_train_selected_normal_plus_attck,1)
-    if processed_train_selected_normal_plus_attck(i,size(processed_train_selected_normal_plus_attck,2))~=1
-        train_labels(i,1) = 1;
-    else
-        train_labels(i,2) = 1;
-    end
+image_ = imresize(input_image,[hight,width]); %resize size(fixd size);
+
+converted_image_2_double = im2double(image_);
+
+% Normalize the Image:
+im_Range = getrangefromclass(converted_image_2_double(1));
+Max_range = im_Range(2);
+Min_range = im_Range(1);
+nomalized_image = (converted_image_2_double - min(converted_image_2_double(:)))*...
+        (Max_range - Min_range)/(max(converted_image_2_double(:)) - min(converted_image_2_double(:))) + Min_range;
+    
+tmp = reshape(nomalized_image, [1,227*227]);
+train_set = zeros(length(tmp),10);
+
+for i=1:10
+    train_set(:,i) = tmp;
 end
-% train_set = [input_nurons , number_of_training_samples]
-% train_labels = [number_of_training_samples , number of output neurons]
-train_set = processed_train_selected_normal_plus_attck(:,1:size(processed_train_selected_normal_plus_attck,2)-1)';
-clear('processed_train_selected_normal_plus_attck')
-%%%%%%%%%%%%%%%%%%%%%%%5
-
-% load('F:\Documents\R\Kyoto\Processed Dataset\preprocessed\mtalab\processed_test_selected_normal_plus_attck.mat');
-
-% p6
-load('F:\Documents\R\Kyoto\Processed Dataset\preprocessed\mtalab\p9\test_kyoto_data.mat');
-processed_test_selected_normal_plus_attck = test_data;
-clear('test_data')
-% test2-1 targets
-test_labels = zeros(2,size(processed_test_selected_normal_plus_attck,1))';
-for i=1:size(processed_test_selected_normal_plus_attck,1)
-    if processed_test_selected_normal_plus_attck(i,size(processed_test_selected_normal_plus_attck,2))~=1
-        test_labels(i,1) = 1;
-    else
-        test_labels(i,2) = 1;
-    end
-end
-test_set = processed_test_selected_normal_plus_attck(:,1:size(processed_test_selected_normal_plus_attck,2)-1)';
-clear('processed_test_selected_normal_plus_attck')
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% load('F:\Documents\MATLAB\Data\Kyoto\test_data2.mat');
-% load('F:\Documents\MATLAB\Data\Kyoto\biased test set from unselected_normal_plus_attck.mat');
-% test_data2 = test_data;
-% clear('test_data')
-% % % tets2-2 targets
-% test_2_labels = zeros(2,size(test_data2,1))';
-% for i=1:size(test_data2,1)
-%     if test_data2(i,size(test_data2,2))==1
-%         test_2_labels(i,1) = 1;
-%     else
-%         test_2_labels(i,2) = 1;
-%     end
-% end
-% test_set2 = test_data2(:,1:size(test_data2,2)-1)';
-% clear('test_data2')
-
-
-
+train_labels = tmp;
 %% input dialog
 prompt = {'Method:(1 for Batch method - 0 for Online method)','Learning rate:','alfa:(Momentum coefficient)'...
-    ,'lambda:(Regularization coefficient)','Validation check','Iteration','Number of samples',...
+    ,'lambda:(Regularization coefficient)','Iteration','Number of samples',...
     'Number of layers','Batch size','tanh or sigmoid(1 for tanh -2  for sigmoid)'};
 dlg_title = 'Input';
 num_lines = 1;
-defaultans = {'1','0.3','0','0','100','300',num2str(size(train_set,2)),'3','10000','1'};
+defaultans = {'0','0.3','0','0','50',num2str(size(train_set,2)),'3','10','2'};
 answer = inputdlg(prompt,dlg_title,num_lines,defaultans);
 
-    %% =========== Create MLP Layer's  =====================================================
-    % struct for layers of MLP
-    layer = struct('Size',[],'wts',[],'z',[],'a',[],'delta',[],'DW',[]...
-        ,'bias',[],'MSE',[],'delta_W',[],'delta_bias',[],'big_delta',[],...
-        'big_delta_bias',[],'delta_W_last',[]);
-    parameter = struct('learning_rate',[],'alfa',[],'lambda',[],'epsilon',[],'method',[]);
-    %% =======================set paremeters==========================
-    %value epsilon use for initialize random weights
-    parameter.epsilon = 0.12;
-    % learning method(online or batch)
-    parameter.method =str2num(answer{1});
-    % learning_rate
-    parameter.learning_rate =str2num(answer{2});
-    % momentum value
-    parameter.alfa =str2num(answer{3});
-    % regularization
-    parameter.lambda =str2num(answer{4});
-    % validation check value
-    validation_check =str2num(answer{5});
-    % epoch number(iteration)
-    iteration =str2num(answer{6});
-    % Number of samples
-    number_of_training_samples =str2num(answer{7});
-    % number of layers
-    L =str2num(answer{8});
-    % batch size(for mini batch training)\n';
-    batch_size =str2num(answer{9});
-    % temp=strcat('Enter tanh(1) or sigmoid(2) (1 for tanh and 2  for sigmoid\n');
-    tanh_or_sigmoid = str2num(answer{10});
-     % training data
+%% =========== Create MLP Layer's  =====================================================
+% struct for layers of MLP
+% layer = struct('Size',[],'wts',[],'z',[],'a',[],'delta',[],'DW',[]...
+%     ,'bias',[],'MSE',[],'delta_W',[],'delta_bias',[],'big_delta',[],...
+%     'big_delta_bias',[],'delta_W_last',[]);
+layer = struct('Size',[],'wts',[],'a',[],'delta',[],...
+    'bias',[],'MSE',[],'delta_W',[],'big_delta',[],...
+    'big_delta_bias',[],'delta_W_last',[]);
+parameter = struct('learning_rate',[],'alfa',[],'lambda',[],'epsilon',[],'method',[]);
+%% =======================set paremeters==========================
+%value epsilon use for initialize random weights
+parameter.epsilon = 0.12;
+% learning method(online or batch)
+parameter.method =str2num(answer{1});
+% learning_rate
+parameter.learning_rate =str2num(answer{2});
+% momentum value
+parameter.alfa =str2num(answer{3});
+% regularization
+parameter.lambda =str2num(answer{4});
+% epoch number(iteration)
+iteration =str2num(answer{5});
+% Number of samples
+number_of_training_samples =str2num(answer{6});
+% number of layers
+L =str2num(answer{7});
+% batch size(for mini batch training)\n';
+batch_size =str2num(answer{8});
+% temp=strcat('Enter tanh(1) or sigmoid(2) (1 for tanh and 2  for sigmoid\n');
+tanh_or_sigmoid = str2num(answer{9});
+% training data
 %% ===================================================================================================
-    % train_set = [number_of_input_neurons , number_of_training_samples]
-    number_of_input_neurons = size(train_set,1);
-    
-    layer(1).Size=number_of_input_neurons;
-    layer(1).a.Size=[layer(1).Size];
-    layer(1).a.Type='double';
-    layer(1).a.Range=[];
-    layer(1).a = zeros(layer(1).Size,number_of_training_samples);
-    
-    layer(1).a = train_set(1:number_of_input_neurons,1:number_of_training_samples);
+% train_set = [number_of_input_neurons , number_of_training_samples]
+number_of_input_neurons = size(train_set,1);
+
+layer(1).Size=number_of_input_neurons;
+layer(1).a.Size=[layer(1).Size];
+layer(1).a.Type='double';
+layer(1).a.Range=[];
+layer(1).a = zeros(layer(1).Size,number_of_training_samples);
+
+layer(1).a = train_set(1:number_of_input_neurons,1:number_of_training_samples);
 %     clear('train_set')
 %     layer(1).a(1:layer(1).Size,1:samples) = images(1:layer(1).Size,1:samples);
-    
-    % set layers parameters
-    for c=2:L
-        if(c==L)
-            temp=strcat('Enter numbers of OUTPUTS (number of neuron in last layer) \n');
-        else
-            temp=strcat('Enter numbers of nodes layer',num2str(c),'\n');
-        end
-        
-        layer(c).Size=input(temp);
-        layer(c).wts.Size=[layer(c-1).Size+1,layer(c).Size];
-        layer(c).wts.Type='double';
-        layer(c).wts.Range=[];
-        layer(c).wts = rand(layer(c-1).Size+1,layer(c).Size) * 2 * parameter.epsilon - parameter.epsilon;
-        
-        layer(c).big_delta.Size=[layer(c-1).Size+1,layer(c).Size];
-        layer(c).big_delta.Type='double';
-        layer(c).big_delta.Range=[];
-        layer(c).big_delta = zeros(layer(c-1).Size+1,layer(c).Size);
-        
-        
-        layer(c).delta_W_last.Size=[layer(c-1).Size+1,layer(c).Size];
-        layer(c).delta_W_last.Type='double';
-        layer(c).delta_W_last.Range=[];
-        layer(c).delta_W_last = zeros(layer(c-1).Size+1,layer(c).Size);
-        
-        layer(c).DW.Size=[layer(c-1).Size+1,layer(c).Size];
-        layer(c).DW.Type='double';
-        layer(c).DW.Range=[];
-        layer(c).DW = zeros(layer(c-1).Size+1,layer(c).Size);
-        
-        layer(c).delta.Size=[layer(c).Size];
-        layer(c).delta.Type='double';
-        layer(c).delta.Range=[];
-        layer(c).delta=zeros(1,layer(c).Size);
-        
-        
-        layer(c).big_delta_bias.Size=[layer(c).Size];
-        layer(c).big_delta_bias.Type='double';
-        layer(c).big_delta_bias.Range=[];
-        layer(c).big_delta_bias=zeros(1,layer(c).Size);
-        
-        layer(c).z.Size=[layer(c).Size];
-        layer(c).z.Type='double';
-        layer(c).z.Range=[];
-        layer(c).z = zeros(1,layer(c).Size);
-        
-        layer(c).a.Size=[layer(c).Size];
-        layer(c).a.Type='double';
-        layer(c).a.Range=[];
-        layer(c).a = zeros(1,layer(c).Size);
-        
-        layer(c).bias.Size=1;
-        layer(c).bias.Type='double';
-        layer(c).bias.Range=[];
-        layer(c).bias=1;
-        
-        layer(c).delta_bias.Size=1;
-        layer(c).delta_bias.Type='double';
-        layer(c).delta_bias.Range=[];
-        layer(c).delta_bias=zeros();
-        
-    end
-    
-    tic;
-    
-    %% =========== Forward and  Train MLP =============
-   
-    % Training
-    Train;
 
+% set layers parameters
+for c=2:L
+    temp=strcat('Enter numbers of nodes layer',num2str(c),'\n');
+
+    layer(c).Size=input(temp);
+    layer(c).wts.Size=[layer(c-1).Size+1,layer(c).Size];
+    layer(c).wts.Type='double';
+    layer(c).wts.Range=[];
+    layer(c).wts = rand(layer(c-1).Size+1,layer(c).Size) * 2 * parameter.epsilon - parameter.epsilon;
+    
+    layer(c).big_delta.Size=[layer(c-1).Size+1,layer(c).Size];
+    layer(c).big_delta.Type='double';
+    layer(c).big_delta.Range=[];
+    layer(c).big_delta = zeros(layer(c-1).Size+1,layer(c).Size);
+    
+    
+    layer(c).delta_W_last.Size=[layer(c-1).Size+1,layer(c).Size];
+    layer(c).delta_W_last.Type='double';
+    layer(c).delta_W_last.Range=[];
+    layer(c).delta_W_last = zeros(layer(c-1).Size+1,layer(c).Size);
+
+    layer(c).delta.Size=[layer(c).Size];
+    layer(c).delta.Type='double';
+    layer(c).delta.Range=[];
+    layer(c).delta=zeros(1,layer(c).Size);
+    
+    
+    layer(c).big_delta_bias.Size=[layer(c).Size];
+    layer(c).big_delta_bias.Type='double';
+    layer(c).big_delta_bias.Range=[];
+    layer(c).big_delta_bias=zeros(1,layer(c).Size);
+    
+    layer(c).a.Size=[layer(c).Size];
+    layer(c).a.Type='double';
+    layer(c).a.Range=[];
+    layer(c).a = zeros(1,layer(c).Size);
+    
+    layer(c).bias.Size=1;
+    layer(c).bias.Type='double';
+    layer(c).bias.Range=[];
+    layer(c).bias=1;
+
+    
+end
+
+%% Add mirror layer
+%     i=0;
+% for c=(L+1):(2*L)
+%     layer(c) = layer(L-i);
+%     i = i + 1 ;
 % end
+  i=0;
+for c=(L+1):(2*L)
+    
+%     layer(c) = layer(L-i);
+    
+    layer(c).Size=layer(L-i).Size;
+    layer(c).wts.Size=[layer(c-1).Size+1,layer(c).Size];
+    layer(c).wts.Type='double';
+    layer(c).wts.Range=[];
+    layer(c).wts = rand(layer(c-1).Size+1,layer(c).Size) * 2 * parameter.epsilon - parameter.epsilon;
+    
+    layer(c).big_delta.Size=[layer(c-1).Size+1,layer(c).Size];
+    layer(c).big_delta.Type='double';
+    layer(c).big_delta.Range=[];
+    layer(c).big_delta = zeros(layer(c-1).Size+1,layer(c).Size);
+    
+    
+    layer(c).delta_W_last.Size=[layer(c-1).Size+1,layer(c).Size];
+    layer(c).delta_W_last.Type='double';
+    layer(c).delta_W_last.Range=[];
+    layer(c).delta_W_last = zeros(layer(c-1).Size+1,layer(c).Size);
 
-   
+    layer(c).delta.Size=[layer(c).Size];
+    layer(c).delta.Type='double';
+    layer(c).delta.Range=[];
+    layer(c).delta=zeros(1,layer(c).Size);
+    
+    
+    layer(c).big_delta_bias.Size=[layer(c).Size];
+    layer(c).big_delta_bias.Type='double';
+    layer(c).big_delta_bias.Range=[];
+    layer(c).big_delta_bias=zeros(1,layer(c).Size);
+    
+    layer(c).a.Size=[layer(c).Size];
+    layer(c).a.Type='double';
+    layer(c).a.Range=[];
+    layer(c).a = zeros(1,layer(c).Size);
+    
+    layer(c).bias.Size=1;
+    layer(c).bias.Type='double';
+    layer(c).bias.Range=[];
+    layer(c).bias=1;
     
     
     
-    
-    
-    
+    i = i + 1 ;
+end
+
+
+% 
+L = 2*L;
+
+tic;
+
+%% =========== Forward and  Train MLP =============
+
+% Training
+Train;
+
+
+
+
+
+
+
+
+
 
 
--- Autoencoder/Train.m
+++ Autoencoder/Train.m
@@ -7,13 +7,13 @@ delta_W_last=zeros();
 delta_theta=zeros();
 delta_theta_last=zeros();
 
-% Accuracy_Train = zeros(1,iteration);
-Accuracy_Train = struct('acc',[],'TP',[],'TN',[],'FP',[],'FN',[]);
-% Accuracy_Test = zeros(1,iteration);
-Accuracy_Test = struct('acc',[],'TP',[],'TN',[],'FP',[],'FN',[]);
-% Accuracy_Validation = zeros(1,iteration);
-% Accuracy_Test2 = struct('acc',[],'TP',[],'TN',[],'FP',[],'FN',[]);
-check_validation = zeros(1,validation_check);
+% % Accuracy_Train = zeros(1,iteration);
+% Accuracy_Train = struct('acc',[],'TP',[],'TN',[],'FP',[],'FN',[]);
+% % Accuracy_Test = zeros(1,iteration);
+% Accuracy_Test = struct('acc',[],'TP',[],'TN',[],'FP',[],'FN',[]);
+% % Accuracy_Validation = zeros(1,iteration);
+% % Accuracy_Test2 = struct('acc',[],'TP',[],'TN',[],'FP',[],'FN',[]);
+% check_validation = zeros(1,validation_check);
 
 
 samples  =number_of_training_samples;
@@ -25,12 +25,12 @@ MSE = zeros(1,iteration);
 layer(L).MSE = zeros(layer(L).Size,samples);
 
 counter = 0;
-% Create figure
-figure1 = figure;
-num_in =1;
-% Create axes
-axes1 = axes('Parent',figure1);
-hold(axes1,'on');
+% % Create figure
+% figure1 = figure;
+% num_in =1;
+% % Create axes
+% axes1 = axes('Parent',figure1);
+% hold(axes1,'on');
 
 for epoch=1:iteration % forward and update weight's in number of  iterations
     delta_W=zeros();
@@ -38,9 +38,8 @@ for epoch=1:iteration % forward and update weight's in number of  iterations
     for num_in=1:samples
         %% =========== Forward =============
         counter = counter +1;
-        train_target = train_labels(num_in,:);
-        
-        for c=2:L
+        %% For before mirror layer 
+        for c=2:(L/2)
             if c==2
                 layer(c).z = (layer(c-1).a(:,num_in))'*(layer(c).wts(1:end-1,:))+(layer(c).bias*layer(c).wts(end,:));
             else
@@ -58,12 +57,32 @@ for epoch=1:iteration % forward and update weight's in number of  iterations
                 layer(c).a = sigmoid(layer(c).z);
             end
         end
-
-        % =========== Computing MSE  =================
-        layer(L).MSE(:,num_in) = (train_target - layer(L).a).^2;
+        %% Froward for mirror layers
+        for c=((L/2)+1):L
+%             if c==2
+%                 layer(c).z = (layer(c-1).a(:,num_in))'*(layer(c).wts(1:end-1,:))+(layer(c).bias*layer(c).wts(end,:));
+%             else
+                layer(c).z = (layer(c-1).a(:))'*(layer(c).wts(1:end-1,:))+(layer(c).bias*layer(c).wts(end,:));
+%             end
+            
+            if isnan(layer(c).z)
+                fprintf('\n');fprintf('\n'); fprintf('Please Change input parameter to prevent NaN in Calculation');
+                error('Error We Find NaN in Calculation!!!!!!!!!!!!!!!');
+            end
+            
+            if tanh_or_sigmoid==1 %tanh
+                layer(c).a = tanhyp(layer(c).z);
+            else %sigmoid
+                layer(c).a = sigmoid(layer(c).z);
+            end
+        end
+        
+        
+        %% =========== Computing MSE  =================
+        layer(L).MSE(:,num_in) = (train_labels - layer(L).a).^2;
         %% =========== Computing Delta's  =============
-        layer(L).delta = -(train_target - layer(L).a);
-       
+        layer(L).delta = -(train_labels - layer(L).a);
+        
         % Compute Other Delta's
         hl=L-1;
         while(hl>1)
@@ -169,151 +188,97 @@ for epoch=1:iteration % forward and update weight's in number of  iterations
     
     %%
     
-    
     %% TEST & Accuracy & MSE
     
-    Train_or_Test = 0;% accuracy on Train Data
-    train_result = Test(layer,samples,Train_or_Test,L,train_labels,train_set,tanh_or_sigmoid);
+    %     Train_or_Test = 0;% accuracy on Train Data
+    %     Accuracy_Train(epoch) = Test(layer,samples,Train_or_Test,L,labels,images,tanh_or_sigmoid)/samples;
+    %     %
     %
-    Accuracy_Train(epoch).acc = (train_result.TP + train_result.TN) /samples;
-    Accuracy_Train(epoch).TP = train_result.TP;
-    Accuracy_Train(epoch).TN = train_result.TN;
-    Accuracy_Train(epoch).FP = train_result.FP;
-    Accuracy_Train(epoch).FN = train_result.FN;
-    
-    Train_or_Test = 1;% accuracy on Test Data
-    test_result = Test(layer,size(test_set,2),Train_or_Test,L,test_labels,test_set,tanh_or_sigmoid);
     %
-    Accuracy_Test(epoch).acc = (test_result.TP + test_result.TN) /size(test_set,2);
-    Accuracy_Test(epoch).TP = test_result.TP;
-    Accuracy_Test(epoch).TN = test_result.TN;
-    Accuracy_Test(epoch).FP = test_result.FP;
-    Accuracy_Test(epoch).TN = test_result.TN;
-    
-%         Train_or_Test = 2;% accuracy on Validation Data
-%     test2_result = Test(layer,size(test_set2,2),Train_or_Test,L,test_2_labels,...
-%         test_set2,tanh_or_sigmoid);
-%     Accuracy_Test2(epoch).acc = (test2_result.TP + test2_result.TN) /size(test_set2,2); 
-%     Accuracy_Test(epoch).TP = test2_result.TP;
-%     Accuracy_Test(epoch).TN = test2_result.TN;
-%     Accuracy_Test(epoch).FP = test2_result.FP;
-%     Accuracy_Test(epoch).TN = test2_result.TN;
-    
+    %     Train_or_Test = 1;% accuracy on Test Data
+    %     Accuracy_Test(epoch) = Test(layer,tsamples,Train_or_Test,L,tlabels,timages,tanh_or_sigmoid)/tsamples;
+    %     %
+    %     Train_or_Test = 2;% accuracy on Validation Data
+    %     Accuracy_Validation(epoch) = Test(layer,tsamples_v,Train_or_Test,L,validation_labels,...
+    %         validation_images,tanh_or_sigmoid)/tsamples_v;
+    %
     % Compute Overal MSE
-    MSE(epoch) = (sum(layer(L).MSE(:))^0.5) / samples;
-    
-    
-    
-%     if epoch >1
-%         
-%         % Create multiple lines using matrix input to plot
-%         t1(1:epoch-1) = Accuracy_Train(1:epoch-1).acc;
-%         % Create multiple lines using matrix input to plot
-%         plot1 = plot(t1(:));hold on;
-%         set(plot1,'DisplayName','Accuracy Train','LineWidth',4,'LineStyle',':',...
-%             'Color',[0.600000023841858 0.200000002980232 0]);
-%         t2(1:epoch-1) = Accuracy_Test(1:epoch-1).acc;
-%         plot2 = plot(t2(:));hold on;
-%         set(plot2,'DisplayName','Accuracy Test 1','LineWidth',3,'Color',[0 1 0]);
-%         
-% %         plot3 = plot(Accuracy_Test2(1:epoch-1).acc);hold on;
-% %         set(plot3,'DisplayName','Accuracy Test 2','LineWidth',3,'LineStyle','-.',...
-% %             'Color',[0 0 0.5]);
-%         
-%         % Create xlabel
-%         xlabel({'Iteration'});
-% 
-%         % Create ylabel
-%         ylabel({'Accuracy'});
-% 
-%         % Create title
-%         title({'Accuracy per Iteration'});
-% 
-%         % Set the remaining axes properties
-%         set(axes1,'XGrid','on','YGrid','on');
-%         
-%         % Create legend
-%         legend('show');
-%         set(legend,...
-%             'Position',[0.139580285377526 0.818740401582967 0.118594433997767 0.0839733720985485]);
-%         drawnow
-%     end
-    
-    %     close all;
-    
-    % adaptive learning rate
-    %     if epoch>1
-    %         if (Accuracy_Train(epoch)/Accuracy_Train(epoch-1))<1
-    %             parameter.learning_rate = 1.05*parameter.learning_rate;
-    %         elseif ((Accuracy_Train(epoch)/Accuracy_Train(epoch-1))>=1) & ((Accuracy_Train(epoch)/Accuracy_Train(epoch-1))<=1.04)
-    %             parameter.learning_rate = parameter.learning_rate;
-    %         else
-    %             parameter.learning_rate = 0.7*parameter.learning_rate;
-    %         end
-    %     end
-    
-    %     % or
-    %      if epoch>1
-    %         if (Accuracy_Train(epoch)/Accuracy_Train(epoch-1))<1
-    %             parameter.learning_rate = 0.7*parameter.learning_rate;
-    %         elseif ((Accuracy_Train(epoch)/Accuracy_Train(epoch-1))>=1) & ((Accuracy_Train(epoch)/Accuracy_Train(epoch-1))<=1.04)
-    %             parameter.learning_rate = parameter.learning_rate;
-    %         else
-    %             parameter.learning_rate = 1.05*parameter.learning_rate;
-    %         end
-    %     end
-    
-    % or
-%     if epoch>1
-%         if (Accuracy_Test(epoch).acc/Accuracy_Test(epoch-1).acc)<1
-%             parameter.learning_rate = 0.7 * parameter.learning_rate;
-%             parameter.alfa = 0.7 * parameter.alfa;
-%         elseif ((Accuracy_Test(epoch).acc/Accuracy_Test(epoch-1).acc)>=1) & ((Accuracy_Test(epoch).acc/Accuracy_Test(epoch-1).acc)<=1.04)
-%             parameter.learning_rate = parameter.learning_rate;
-%             parameter.alfa =  parameter.alfa;
-%         else
-%             parameter.learning_rate = 1.05*parameter.learning_rate;
-%             parameter.alfa = 1.05 *parameter.alfa;
-%         end
-%     end
-%     %% Validation check
-%     if epoch == 1
-%         validation = Accuracy_Validation(epoch);
-%     end
-    
-%     if Accuracy_Validation(epoch)<=validation
-%         index = index+1;
-%     else
-%         validation = Accuracy_Validation(epoch);
-%         index = 0;
-%     end
-    
-%     if index == validation_check
-%         break;
-%     end
-    
     %%
+    MSE(epoch) = (sum(layer(L).MSE(:))^0.5) / samples;
+    fprintf(num2str(MSE(epoch)));
+    fprintf('\n');
+    %%
+    % if epoch >1
+    %
+    %         % Create multiple lines using matrix input to plot
+    %         plot1 = plot(Accuracy_Train(1:epoch-1));hold on;
+    %         set(plot1,'DisplayName','Accuracy Train','LineWidth',4,'LineStyle',':',...
+    %             'Color',[0.600000023841858 0.200000002980232 0]);
+    %
+    %         plot2 = plot(Accuracy_Test(1:epoch-1));hold on;
+    %         set(plot2,'DisplayName','Accuracy Test','LineWidth',3,'Color',[0 1 0]);
+    %
+    %         plot3 = plot(Accuracy_Validation(1:epoch-1));hold on;
+    %         set(plot3,'DisplayName','Accuracy Validation','LineWidth',3,'LineStyle','-.',...
+    %             'Color',[0 0 0.5]);
+    %
+    %
+    %         % Set the remaining axes properties
+    %         set(axes1,'XGrid','on','YGrid','on');
+    %
+    %         % Create legend
+    %         legend('show');
+    %         set(legend,...
+    %             'Position',[0.139580285377526 0.818740401582967 0.118594433997767 0.0839733720985485]);
+    %         drawnow
+    % end
+    
+    % % or
+    %     if epoch>1
+    %         if (Accuracy_Test(epoch)/Accuracy_Test(epoch-1))<1
+    %             parameter.learning_rate = 0.7 * parameter.learning_rate;
+    %             parameter.alfa = 0.7 * parameter.alfa;
+    %         elseif ((Accuracy_Test(epoch)/Accuracy_Test(epoch-1))>=1) & ((Accuracy_Test(epoch)/Accuracy_Test(epoch-1))<=1.04)
+    %             parameter.learning_rate = parameter.learning_rate;
+    %             parameter.alfa =  parameter.alfa;
+    %         else
+    %             parameter.learning_rate = 1.05*parameter.learning_rate;
+    %             parameter.alfa = 1.05 *parameter.alfa;
+    %         end
+    %     end
+    %     %% Validation check
+    %     if epoch == 1
+    %         validation = Accuracy_Validation(epoch);
+    %     end
+    %
+    %     if Accuracy_Validation(epoch)<=validation
+    %         index = index+1;
+    %     else
+    %         validation = Accuracy_Validation(epoch);
+    %         index = 0;
+    %     end
+    %
+    %     if index == validation_check
+    %         break;
+    %     end
+    
+    
+    
+    
+    
 end
 
-% t1(1:epoch-1) = Accuracy_Train(1:epoch-1).acc;
+
+
 % % Create multiple lines using matrix input to plot
-% plot1 = plot(t1(:));hold on;
+% plot1 = plot(Accuracy_Train(1:epoch-1));hold on;
 % set(plot1,'DisplayName','Accuracy Train','LineWidth',4,'LineStyle',':',...
 %     'Color',[0.600000023841858 0.200000002980232 0]);
-% t2(1:epoch-1) = Accuracy_Test(1:epoch-1).acc;
-% plot2 = plot(t2(:));hold on;
-% set(plot2,'DisplayName','Accuracy Test 1','LineWidth',3,'Color',[0 1 0]);
-% % plot3 = plot(Accuracy_Test2(1:epoch-1).acc);hold on;
-% % set(plot3,'DisplayName','Accuracy Test 2','LineWidth',3,'LineStyle','-.',...
-% %     'Color',[0 0 0.5]);
-% % Create xlabel
-% xlabel({'Iteration'});
-% 
-% % Create ylabel
-% ylabel({'Accuracy'});
-% 
-% % Create title
-% title({'Accuracy per Iteration'});
+% plot2 = plot(Accuracy_Test(1:epoch-1));hold on;
+% set(plot2,'DisplayName','Accuracy Test','LineWidth',3,'Color',[0 1 0]);
+% plot3 = plot(Accuracy_Validation(1:epoch-1));hold on;
+% set(plot3,'DisplayName','Accuracy Validation','LineWidth',3,'LineStyle','-.',...
+%     'Color',[0 0 0.5]);
 % % Set the remaining axes properties
 % set(axes1,'XGrid','on','YGrid','on');
 % % Create legend
@@ -322,7 +287,20 @@ end
 %     'Position',[0.139580285377526 0.818740401582967 0.118594433997767 0.0839733720985485]);
 % drawnow;
 
+result = layer(L).a;
+
+tmp2 = reshape(result,[227 227]);
+
+figure;
+imshow(tmp2);
+title('Autoencoder image');
+
+figure;
+imshow(input_image);
+title('Original image');
+%%
 draw_MSE(MSE);
 
 toc;
+
 
