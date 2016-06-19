5cc151d7cb03072c6bf723f58c76b9e410984dfe
Mohsen Rajaie <mrajaie1@gmail.com>
2016-06-19 15:21:25 +0430

    remove useless files

--- EA/bahram_salamat/createArrays.m
+++
@@ -1,6 +0,0 @@
-function result = createArrays(nArrays, arraySize)
-    result = cell(1, nArrays);
-    for i = 1 : nArrays
-        result{i} = zeros(arraySize);
-    end
-end
--- EA/bahram_salamat/evaluation.m
+++
@@ -1,8 +0,0 @@
-function [z]=evaluation(lambda,xm)
-
-z = zeros(1,lambda);
-for i = 1:lambda
-    z(:,i) = func(xm(:,i));
-end
-
-
