1b03a7a079be28ea86f880b00ad3e1c6ef22f2a2
Mohsen Rajaie <mrajaie1@gmail.com>
2016-06-19 15:31:05 +0430

    remove DES of Majazi

--- DES/fileread.m
+++
@@ -1,8 +0,0 @@
-mydata='E:\DES\mydata.txt';
-%text1 = fileread('mydata')
-fid = fopen(mydata, 'r');
-M = fread(fid,'*char')'
-fclose(fid)
-mydata1='E:\DES\mydata1.txt';
-fid1=fopen(mydata1,'w');
-fwrite(fid1,M)
