c717b6acaf8cef6d72041e127f9fc33258058bb3
Mohsen Rajaie <mrajaie1@gmail.com>
2016-07-02 17:13:12 +0430

    performing...

--- MNIST & Hoda/MLP with BP for MNIST & Hoda/Target.m
+++
@@ -1,8 +0,0 @@
-%% =========== Returns the target vector =============
-%   
-function t=Target(n,l)
-
-    t= zeros(1,l);
-    t(n+1)=1;
-    
-end
--- MNIST & Hoda/MLP with BP for MNIST & Hoda/tanhyp.m
+++
@@ -1,6 +0,0 @@
-%tanh(x) = sinh(x)/cosh(x) = ( ex - e-x )/( ex + e-x )
-%% =========== Compute tanh Function =============
-function g = tanhyp(z)
-% g = (exp(z)-exp(-z)) ./ (exp(z)+exp(-z));
-g = tanh(z);
-end
